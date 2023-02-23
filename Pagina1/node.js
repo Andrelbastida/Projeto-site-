51d0fd656d899062bb25c4d59bc7f9dc1055a9d8
eoKelvio <kelvinsc7@gmail.com>
2023-02-22 21:44:51 -0300

    Test_commit

---
+++ Pagina1/node.js
@@ -0,0 +1,10 @@
+var bot = document.getElementById('botao')
+bot.addEventListener('click',magia)
+var pux_html = 0;
+var pux_js = 0;
+
+function magia(){
+    pux_html = document.getElementById('bloco_html');
+    pux_js = document.getElementById('bloco_js');
+
+}
