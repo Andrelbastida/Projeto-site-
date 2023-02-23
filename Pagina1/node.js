cd2395a2d30670d477160217bd30a0023341e8cd
eoKelvio <kelvinsc7@gmail.com>
2023-02-23 19:27:16 -0300

    "btns" corrigido

--- Pagina1/node.js
+++ Pagina1/node.js
@@ -1,5 +1,7 @@
-var bot = document.getElementById('executar')
-bot.addEventListener('click',magia)
+var bot_exec = document.getElementById('executar')
+bot_exec.addEventListener('click',executar)
+var bot_limp = document.getElementById('limpar')
+bot_limp.addEventListener('click',limpar)
 var oct_html = 0;
 var oct_js = 0;
 
@@ -9,20 +11,18 @@ limparTexto.addEventListener("click", textoLimpo)
 var voltarPagina = document.getElementById("voltarPagina")
 voltarPagina.addEventListener('click',voltarPaginaInicial)
 
-function magia(){
+function executar(){
     var oct_html
     var oct_js
+    oct_html = document.getElementById('inserirhtml').style.display = 'none'
+    oct_js = document.getElementById('inserirjs').style.display = 'none'
+}
 
-    
+function limpar(){
+    oct_html = document.getElementById('inserirhtml').style.display = 'block'
+    oct_js = document.getElementById('inserirjs').style.display = 'block'
 
 
-
-
-
-
-
-    oct_html = document.getElementById('').style.display = 'none'
-    oct_js = document.getElementById('').style.display = 'none'
 }
 
     function voltarPaginaInicial(){
