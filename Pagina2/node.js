48095d02e376dbafe1a9542aeea2b1dde6f612d4
eoKelvio <kelvinsc7@gmail.com>
2023-02-26 13:36:46 -0300

    Terminado("mas não ideal")

--- Pagina2/node.js
+++ Pagina2/node.js
@@ -1,35 +1,43 @@
-/*function autoResize()
-    {
-        objTextArea = document.getElementById('codehtml');
-        while (objTextArea.scrollHeight > objTextArea.offsetHeight)
-        {
-            objTextArea.rows += 1;
-        }
-    }
-*/
 var p1 = 0
 var p2 = document.getElementById('page2').style.display = 'none'
-var btn_exec = document.getElementById('btnExecutar')
+var btn_exec = document.getElementById('btnExecutar');
 btn_exec.addEventListener('click', gerar);
-var btn_volt = document.getElementById('btnVoltar')
-btn_volt.addEventListener('click', voltar);
+var btn_limp = document.getElementById('btnLimpar');
+btn_limp.addEventListener('click',limpar)
 
 
 function gerar() {
-    p1 = document.getElementById('page1')
-    p2 = document.getElementById('page2')
+    p1 = document.getElementById('page1');
+    p2 = document.getElementById('page2');
 
     p1.style.display = 'none';
     p2.style.display = 'block';
 
-    var tHtml = document.getElementById('codeHtml').value
-    var tJs = document.getElementById('codeJs').value
+    tHtml = document.getElementById('codeHtml').value;
+    tJs = document.getElementById('codeJs').value;
 
     p2.innerHTML = tHtml 
     document.getElementById('jsUser').innerHTML = tJs;
+
+    var btn_exec = document.getElementById('btnExecutar');
+    btn_exec.addEventListener('click', voltar);
+    btn_exec.removeEventListener('click',gerar);
+    document.getElementById('btnExecutar').value = 'Voltar';
+    
 }
 
 function voltar() {
     mp1 = document.getElementById('page1').style.display = 'block';
     mp2 = document.getElementById('page2').style.display = 'none';
+    var btn_exec = document.getElementById('btnExecutar');
+    btn_exec.addEventListener('click', gerar);
+    btn_exec.removeEventListener('click',voltar);
+    document.getElementById('btnExecutar').value = 'Gerar';
+    location.reload(false);
+}
+
+function limpar(){
+    document.getElementById('codeHtml').value ='';
+    document.getElementById('codeJs').value = '';
+    document.getElementById('jsUser').innerHTML = '';
 }
